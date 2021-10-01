c284fb397b71a9e1ec35235ab179b84a91f0347e
githubVivek55 <vivekpce55@outlook.com>
2021-10-01 23:11:16 +0530

    CryptoApp- Add  navigation menu and page components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
-import {Navbar} from './components';
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from './components';
 import './style.css';
 
 function App () {
@@ -10,8 +17,43 @@ function App () {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main" />
-      <div className="footer" />
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{color: 'white', textAlign: 'center'}}
+          >
+            Crypto app <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CryptoDetails () {
+  return (
+    <div>
+      Crypto details
+    </div>
+  );
+}
+
+export default CryptoDetails;
