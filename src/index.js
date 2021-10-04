3b1f7ab77225491b2cf3d3cfcfe64a2880282e59
githubVivek55 <vivekpce55@outlook.com>
2021-10-04 23:14:43 +0530

    CryptoApp- Add crypto currencies list to home page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
-import 'antd/dist/antd.css';
 
-ReactDOM.render (<Router><App /></Router>, document.getElementById ('root'));
+import 'antd/dist/antd.css';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
+
+console.log (store);
+
+ReactDOM.render (
+  <Router><Provider store={store}><App /></Provider></Router>,
+  document.getElementById ('root')
+);
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoAPIHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'f3e294e8d2mshc47c54fa4b6b6c5p10aa3djsnbd8f9bfe9182',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = url => ({url, headers: cryptoAPIHeaders});
+
+export const cryptoApi = createApi ({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery ({baseUrl}),
+  endpoints: builder => ({
+    getCryptos: builder.query ({
+      query: () => createRequest ('/coins'),
+    }),
+  }),
+});
+
+export const {useGetCryptosQuery} = cryptoApi;
